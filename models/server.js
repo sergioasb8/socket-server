c01b02f672a6034ea7c2cf5690372b8b01018828
sergioasb8 <sergioasb8@gmail.com>
2021-05-31 17:11:00 -0500

    cors

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const express   = require('express');
 const http      = require('http');
 const socketio  = require('socket.io');
 const path      = require('path');
+const cors      = require('cors');
 const Sockets   = require('./sockets');
 
 
@@ -23,8 +24,12 @@ class Server {
     }
 
     middlewares() {
+
         // Display the public directory 
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
+
+        // CORS
+        this.app.use( cors() );
     }
 
     configSockets() {
